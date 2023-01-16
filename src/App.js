8173c40bdb719372188dbf17dcfee3d5f92cbf4a
Danilo Amaral Ribeiro <daniloamaralr@gmail.com>
2023-01-16 11:55:03 -0300

    create a transferItem component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
+import TransferItem from "./components/TransferItem";
+
 function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
+      <TransferItem></TransferItem>
     </div>
   );
 }
---
+++ src/components/TransferItem.js
@@ -0,0 +1,13 @@
+function TransferItem() {
+  return (
+    <div>
+      <div> Jan 3rd 2023</div>
+      <div>
+        <h2>Gerson - Olympique - Flamengo</h2>
+        <div> $15M</div>
+      </div>
+    </div>
+  );
+}
+
+export default TransferItem;
