a11c873379d1bb301a0a2e4852c4aaeaa5914662
Danilo Amaral Ribeiro <daniloamaralr@gmail.com>
2023-01-19 11:10:19 -0300

    changing show form logic

--- src/components/NewTransfer/NewTransfer.js
+++ src/components/NewTransfer/NewTransfer.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import TransferForm from "./TransferForm";
 
 import "./NewTransfer.css";
 const NewTransfer = (props) => {
+  const [isEditing, setIsEditing] = useState(false);
   const saveTransferDataHandler = (enteredTransfer) => {
     // console.log(enteredTransfer);
     const newTransfer = {
       ...enteredTransfer,
       id: Math.random().toString(),
     };
-    props.onAddTransfer(newTransfer)
+    props.onAddTransfer(newTransfer);
+    setIsEditing(false);
+  };
+
+  const startEditingHandler = () => {
+    setIsEditing(true);
+  };
+
+  const stopEditingHandler = () => {
+    setIsEditing(false);
   };
 
   return (
     <div className="new-transfer">
-      <TransferForm onSaveNewTransfer={saveTransferDataHandler} />
+      {!isEditing && (
+        <button onClick={startEditingHandler}> Add New Transfer</button>
+      )}
+      {isEditing && (
+        <TransferForm onSaveNewTransfer={saveTransferDataHandler} onCancel={stopEditingHandler}/>
+      )}
     </div>
   );
 };
--- src/components/NewTransfer/TransferForm.js
+++ src/components/NewTransfer/TransferForm.js
@@ -6,8 +6,6 @@ const TransferForm = (props) => {
   const [enteredPrice, setEnteredPrice] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
 
-  const [isClickedButtonAddNewTransfer, setIsClickedButtonAddNewTransfer] =
-    useState(false);
   // const [userInput, setUserInput] = useState({
   //   enteredTitle: "",
   //   enteredPrice: "",
@@ -41,59 +39,48 @@ const TransferForm = (props) => {
       date: new Date(enteredDate),
     };
 
-    props.onSaveNewTransfer(transferData);
+    props.onSaveNewTransfer(transferData)
     setEnteredTitle("");
     setEnteredPrice("");
     setEnteredDate("");
   };
 
-  const onButtonClickedHandler = (event) => {
-    setIsClickedButtonAddNewTransfer(!isClickedButtonAddNewTransfer);
-  };
-
   return (
     <form onSubmit={submitHandler}>
-      {!isClickedButtonAddNewTransfer && (
-        <div className="new-transfer__actions">
-          <button onClick={onButtonClickedHandler}> Add New Transfer</button>
+      <div className="new-transfer__controls">
+        <div className="new-transfer__control">
+          <label>Title</label>
+          <input
+            type="text"
+            onChange={titleChangeHandler}
+            value={enteredTitle}
+          />
         </div>
-      )}
-      {isClickedButtonAddNewTransfer && (
-        <div className="new-transfer__controls">
-          <div className="new-transfer__control">
-            <label>Title</label>
-            <input
-              type="text"
-              onChange={titleChangeHandler}
-              value={enteredTitle}
-            />
-          </div>
-          <div className="new-transfer__control">
-            <label>Price</label>
-            <input
-              type="number"
-              min="0.01"
-              step="0.01"
-              value={enteredPrice}
-              onChange={priceChangeHandler}
-            />
-          </div>
-          <div className="new-transfer__control">
-            <label>Date</label>
-            <input
-              type="date"
-              min="2019-01-01"
-              max="2022-12-31"
-              value={enteredDate}
-              onChange={dateChangeHandler}
-            />
-          </div>
-          <div className="new-transfer__actions">
-            <button onClick={onButtonClickedHandler}>Cancel</button>
-            <button type="submit">Add Transfer</button>
-          </div>
+        <div className="new-transfer__control">
+          <label>Price</label>
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            value={enteredPrice}
+            onChange={priceChangeHandler}
+          />
         </div>
-      )}
+        <div className="new-transfer__control">
+          <label>Date</label>
+          <input
+            type="date"
+            min="2019-01-01"
+            max="2022-12-31"
+            value={enteredDate}
+            onChange={dateChangeHandler}
+          />
+        </div>
+      </div>
+      <div className="new-transfer__actions">
+      <button type="button" onClick={props.onCancel}>Cancel</button>
+        <button type="submit">Add Transfer</button>
+      </div>
     </form>
   );
 };
